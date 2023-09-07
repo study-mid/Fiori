12ad6d61d4644e3040680226f1e3e7501281b6e6
PARK-mid <hwp45_76@naver.com>
2023-09-07 11:48:22 +0900

    230907_9/6복습

--- project_b12_06/webapp/controller/Main.controller.js
+++ project_b12_06/webapp/controller/Main.controller.js
@@ -1,15 +1,59 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller) {
-        "use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/ui/core/Fragment"],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller, Fragment) {
+    "use strict";
 
-        return Controller.extend("projectb1206.controller.Main", {
-            onInit: function () {
+    return Controller.extend("projectb1206.controller.Main", {
+      formatter: {
+        fnDateString: function (oDate) {
+          if (oDate) {
+            var oDateTimeInstance =
+              sap.ui.core.foramt.DateFormat.getDateTimeInstance({
+                pattern: "yyyy-MM-dd",
+              });
 
-            }
-        });
+            return oDateTimeInstance.foramt(oDate);
+          }
+        },
+      },
+
+      onInit: function () {},
+
+      onValueHelp: function () {
+        var oDialog = this.byId("idDialog");
+        var oModel = this.getView().getModel();
+        //manifest.json 의 모델을 가져오게된다.
+        // var oDialog = this.byId("idDialog");
+        // oOrderID = this.byId("OrderID").getValue();
+        // oCustomerID = this.byId("CustomerID").getValue();
+        // oEmployeeID = this.byId("EmployeeID").getValue();
+
+        if (oDialog) {
+          oDialog.open();
+        } else {
+          this.loadFragment({
+            // this는 현재 controller
+            name: "projectb1206.view.fragment.Order",
+            type: "XML",
+            // mvc.controller-methods의 소스코드
+          }).then(
+            function (oDialog) {
+              oDialog.setModel(oModel);
+              oDialog.open();
+            }.bind(this)
+          );
+        }
+      },
+
+      onClose: function (oEvent) {
+        var oButton = oEvent.getSource();
+        var oDialog = oButton.getParent();
+
+        oDialog.close();
+      },
     });
+  }
+);
