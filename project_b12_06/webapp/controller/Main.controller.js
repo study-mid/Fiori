0cf76ccb837bef7f19b1e682a58d780aab91f2c4
PARK-mid <hwp45_76@naver.com>
2023-09-08 12:01:39 +0900

    230908_filter적용

--- project_b12_06/webapp/controller/Main.controller.js
+++ project_b12_06/webapp/controller/Main.controller.js
@@ -1,9 +1,14 @@
 sap.ui.define(
-  ["sap/ui/core/mvc/Controller", "sap/ui/core/Fragment"],
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/Fragment",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+  ],
   /**
    * @param {typeof sap.ui.core.mvc.Controller} Controller
    */
-  function (Controller, Fragment) {
+  function (Controller, Fragment, Filter, FilterOperator) {
     "use strict";
 
     return Controller.extend("projectb1206.controller.Main", {
@@ -11,7 +16,7 @@ sap.ui.define(
         fnDateString: function (oDate) {
           if (oDate) {
             var oDateTimeInstance =
-              sap.ui.core.foramt.DateFormat.getDateTimeInstance({
+              sap.ui.core.format.DateFormat.getDateTimeInstance({
                 pattern: "yyyy-MM-dd",
               });
 
@@ -31,28 +36,63 @@ sap.ui.define(
         // oCustomerID = this.byId("CustomerID").getValue();
         // oEmployeeID = this.byId("EmployeeID").getValue();
 
-        if (oDialog) {
-          oDialog.open();
-        } else {
-          this.loadFragment({
-            // this는 현재 controller
+        if (!oDialog) {
+          Fragment.load({
             name: "projectb1206.view.fragment.Order",
             type: "XML",
-            // mvc.controller-methods의 소스코드
-          }).then(
-            function (oDialog) {
-              oDialog.setModel(oModel);
-              oDialog.open();
-            }.bind(this)
-          );
+            controller: this,
+          }).then(function (oDialog) {
+            oDialog.setModel(oModel);
+            oDialog.open();
+          });
+        } else {
+          oDialog.open();
         }
+        // this.loadFragment({
+        //   // this는 현재 controller
+        //   name: "projectb1206.view.fragment.Order",
+        //   type: "XML",
+        //   // mvc.controller-methods의 소스코드
+        // }).then(
+        //   function (oDialog) {
+        //     oDialog.setModel(oModel);
+        //     oDialog.open();
+        //   }.bind(this)
+        // );
       },
 
       onClose: function (oEvent) {
-        var oButton = oEvent.getSource();
-        var oDialog = oButton.getParent();
+        var oDialog = oEvent.getSource().getParent();
 
         oDialog.close();
+      },
+
+      onBeforeOpen: function () {
+        var oTable = sap.ui.getCore().byId("idOrderTable");
+        var aFilters = [
+          new Filter({
+            path: "EmployeeID",
+            operator: "GE", // FilterOperator.GE
+            value1: 4,
+          }),
+          new Filter({
+            path: "CustomerID",
+            operator: "Contains", // FilterOperator.Contains
+            value1: "R",
+          }),
+        ];
+        var oFilter = new Filter({
+          path: "EmployeeID",
+          operator: "GE",
+          value1: 4,
+        });
+        // oTable의 rows에 바인딩된 정보를 가져와서
+        // 바인딩 정보 중 filter 안에 필터 객체를 추가
+        // -> 이 때 filter() 안에는 Object 또는 Array 형태가 들어 갈 수 있음
+        // oTable.getBinding('rows').filter() => 이렇게 하면 필터 초기화
+
+        oTable.getBinding("rows").filter(aFilters);
+        debugger;
       },
     });
   }
